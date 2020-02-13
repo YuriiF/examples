2daad85016fef6a3b2095915d6bfe9bdbdd579f5
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-13 16:01:04 +0100

    Add some modules for, react-apollo integration, check email, some types, support for sqlite3 -> will be change to nedb

---
+++ apps/client/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    name: 'Apollo Fullstack example',
+    service: 'apollo-example-nrwl',
+  },
+};
--- tools/gen-commands.sh
+++ tools/gen-commands.sh
@@ -4,3 +4,10 @@ nx g @nrwl/react:application --routing=true --pascalCaseFiles=true --style=@emot
 # command to generate node application with defined frontend project
 # that needs access this node app
 nx g @nrwl/node:application --frontendProject=client
+
+# npm module 'apollo' have to be installed 'yarn add apollo --dev'
+# Command to push schema - Apollo Manager
+y apollo service:push --endpoint=http://localhost:4000/ --config=./apps/server/apollo.config.js
+
+# Command to check schema - Apollo Manager
+y apollo service:check --endpoint=http://localhost:4000/ --config=./apps/server/apollo.config.js
