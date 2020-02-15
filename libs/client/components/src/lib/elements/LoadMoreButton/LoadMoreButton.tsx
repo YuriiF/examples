31b356d64658a8aa9144c7776460dbd8b9068e1b
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-15 13:15:06 +0100

    Cleanup

--- libs/client/components/src/lib/elements/LoadMoreButton/LoadMoreButton.tsx
+++ libs/client/components/src/lib/elements/LoadMoreButton/LoadMoreButton.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Button } from 'evergreen-ui';
 
-const StyledLoadMoreButton = styled.div`
-  color: pink;
-`;
-
 export const LoadMoreButton = (props: any) => {
   const { children, ...rest } = props;
   return <Button {...rest}>{children}</Button>;
