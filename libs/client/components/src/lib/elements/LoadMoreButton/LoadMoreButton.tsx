e57b2a4c31bf3c5f74c99462c85fc09d66c5bb98
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-16 20:07:34 +0100

    Fix LoadMore button loding status, implement Cart

--- libs/client/components/src/lib/elements/LoadMoreButton/LoadMoreButton.tsx
+++ libs/client/components/src/lib/elements/LoadMoreButton/LoadMoreButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Button } from 'evergreen-ui';
 
 export const LoadMoreButton = (props: any) => {
-  const { children, ...rest } = props;
-  return <Button {...rest}>{children}</Button>;
+  const { children, isLoading, ...rest } = props;
+  return (
+    <Button isLoading={isLoading} {...rest}>
+      {children}
+    </Button>
+  );
 };
 
 export default LoadMoreButton;
--- libs/client/components/src/lib/pages/Cart/Cart.tsx
+++ libs/client/components/src/lib/pages/Cart/Cart.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, { Fragment } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Loading } from '../../elements/Loading/Loading';
+import { Header } from '../../sections/Header/Header';
+import { BookTrips } from '../../containers/BookTrips/BookTrips';
+import { CartItem } from '../../containers/CartItem/CartItem';
 
-export interface CartProps {}
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
 
-export const Cart = (props: CartProps) => {
-  return <div />;
+export const Cart = () => {
+  const { data, loading, error } = useQuery(GET_CART_ITEMS);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p>ERROR: {error.message}</p>;
+  }
+
+  return (
+    <Fragment>
+      <Header>My Cart</Header>
+      {!data || (!!data && data.cartItems.length === 0) ? (
+        <p data-testid="empty-message">No items in your cart</p>
+      ) : (
+        <Fragment>
+          {!!data &&
+            data.cartItems.map((launchId) => (
+              <CartItem key={launchId} launchId={launchId} />
+            ))}
+
+          <BookTrips cartItems={!!data ? data.cartItems : []} />
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 export default Cart;
