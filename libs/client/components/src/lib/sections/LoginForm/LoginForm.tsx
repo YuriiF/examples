c0b503768fecd22414281e16acdf58deab4de7a3
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-16 20:09:16 +0100

    Implement Login form with useState Hook, little styling, handle values

--- libs/client/components/src/lib/sections/LoginForm/LoginForm.tsx
+++ libs/client/components/src/lib/sections/LoginForm/LoginForm.tsx
@@ -1,23 +1,53 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { Pane, Button, TextInputField } from 'evergreen-ui';
+import { unit, colors } from '../../themes/GlobalStyles/Global';
+
+const StyledForm = styled('form')({
+  width: '100%',
+  maxWidth: 406,
+  padding: unit * 3.5,
+  borderRadius: 4,
+  color: colors.text,
+  backgroundColor: 'white',
+  boxShadow:
+    'inset 0 0 0 1px rgba(67, 90, 111, 0.3), inset 0 1px 2px rgba(67, 90, 111, 0.14)',
+});
 
 export interface LoginFormProps {
   login: any;
 }
 
 export const LoginForm = (props: LoginFormProps) => {
-  console.log('[TLOG]: LoginForm -> props', props);
+  const [email, setEmail] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const email = (event.target as HTMLInputElement).value;
+    setEmail(email);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    props.login({ variables: { email } });
+  };
 
   return (
     <Pane marginTop="default">
-      <TextInputField
-        // onChange={(e) => setState({ value: e.target.value })}
-        onChange={(e) => console.log(e.target.value)}
-        // value={state.value}
-        label={'Valid email address'}
-      />
-      <Button marginRight={16}>Login</Button>
+      <StyledForm onSubmit={onSubmit}>
+        <TextInputField
+          // onChange={(e) => setState({ value: e.target.value })}
+          onChange={onChange}
+          label={'Provide valid email address'}
+          required
+          type="email"
+          name="email"
+          placeholder="E-Mail"
+          data-testid="login-input"
+        />
+        <Button marginRight={16} type="submit">
+          Log in
+        </Button>
+      </StyledForm>
     </Pane>
   );
 };
