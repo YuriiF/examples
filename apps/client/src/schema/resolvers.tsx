eec22bf73fcc135e09d2338176211c8bca122526
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-16 09:41:11 +0100

    Local state, types and resolvers

---
+++ apps/client/src/schema/resolvers.tsx
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+
+  extend type Launch {
+    isInCart: Boolean!
+  }
+
+  extend type Mutation {
+    addOrRemoveFromCart(id: ID!): [ID!]!
+  }
+`;
+
+const resolvers = {};
+
+export { typeDefs, resolvers };
