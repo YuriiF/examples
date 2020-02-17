fde75bea7c95acb4ceeca754655cdf1b4d0e5b0e
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-17 21:41:34 +0100

    Create export point for components, containers add alias to tsconfig move App.tsx to components

--- apps/notes/src/main.tsx
+++ apps/notes/src/main.tsx
@@ -1,13 +1,37 @@
+/**
+ * We can us this line at the top of the file
+ * to solve module.hot.accept TypeScript problem.
+ *
+ * ///<reference types="webpack-env" />
+ */
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { render, unmountComponentAtNode } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app/App';
+import { App } from '@bsc/notes/app';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+
+const load = (messages: any, Component = App) => {
+  render(
+    <BrowserRouter>
+      <Component />
+    </BrowserRouter>,
+    MOUNT_NODE,
+    /** TODO DELETE THIS COMMENT: Just to show callback of render method */
+    () => console.log(`${Component.name} component is mounted!`)
+  );
+};
+
+load(null, App);
+
+/**
+ * (module as any).hot fixes TypeScript error.
+ */
+if ((module as any).hot) {
+  (module as any).hot.accept(['./components/App/App'], () => {
+    unmountComponentAtNode(MOUNT_NODE);
+    const App = require('./components/App/App').default;
+    render(null, App);
+  });
+}
