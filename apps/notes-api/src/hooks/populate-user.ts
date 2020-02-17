d7d0f96ba47c3b587cbecd248fe294f810cee784
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-17 12:47:37 +0100

    Merge user object to note, we will know which user crated the note

--- apps/notes-api/src/hooks/populate-user.ts
+++ apps/notes-api/src/hooks/populate-user.ts
@@ -1,7 +1,34 @@
 import { Hook, HookContext } from '@feathersjs/feathers';
 
-export default (options = {}): Hook => {
+export default (): Hook => {
   return async (context: HookContext) => {
+    /** Get `app`, `method`, `params` and `result` from the hook context */
+    const { app, method, result, params } = context;
+
+    /** Function that adds the user to a single note object */
+    const addUser = async (note: any) => {
+      /**
+       * Get the user based on their id, pass the `params` along so
+       * that we get a safe version of the user data.
+       */
+      const user = await app.service('users').get(note.userId, params);
+
+      /** Merge the note content to include the `user` object. */
+      return {
+        ...note,
+        user,
+      };
+    };
+
+    /** In a find method we need to process the entire page */
+    if (method === 'find') {
+      /** Map all data to include the `user` information */
+      context.result.data = await Promise.all(result.data.map(addUser));
+    } else {
+      /** Otherwise just update the single result */
+      context.result = await addUser(result);
+    }
+
     return context;
   };
 };
