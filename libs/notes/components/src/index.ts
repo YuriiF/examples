c07b1a9d62d94ae154a5438227aaef37d470ca24
Yurii Fotul <fotul.yurii@gmail.com>
2020-02-18 05:32:06 +0100

    Global styles component

--- libs/notes/components/src/index.ts
+++ libs/notes/components/src/index.ts
@@ -1,1 +1,11 @@
-export * from './lib/NotesComponent';
+export * from './lib/templates/PageTemplate/PageTemplate';
+export * from './lib/templates/GenericTemplate/GenericTemplate';
+export * from './lib/pages/NotFoundPage/NotFoundPage';
+export * from './lib/pages/HomePage/HomePage';
+export { default as HomePageLoadable } from './lib/pages/HomePage/Loadable';
+
+/** Export loadable utility to use inside our Notes app */
+export * from './lib/utils/loadable';
+
+/** Export all from global styles */
+export * from './lib/themes/GlobalStyles/Global';
---
+++ libs/notes/components/src/lib/themes/GlobalStyles/Global.tsx
@@ -0,0 +1,43 @@
+// @ts-nocheck
+import React from 'react';
+import { Global, css } from '@emotion/core';
+
+const unit = 8;
+const colors = {
+  primary: '#220a82',
+  secondary: '#14cbc4',
+  accent: '#e535ab',
+  background: '#f7f8fa',
+  grey: '#d8d9e0',
+  text: '#343c5a',
+  textSecondary: '#747790',
+};
+
+const GlobalStyles = () => {
+  return (
+    <Global
+      styles={css({
+        [['html', 'body']]: {
+          height: '100%',
+        },
+        body: {
+          margin: 0,
+          padding: 0,
+          fontFamily: "'Source Sans Pro', sans-serif",
+          backgroundColor: colors.background,
+          color: colors.text,
+        },
+        '#root': {
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100%',
+        },
+        '*': {
+          boxSizing: 'border-box',
+        },
+      })}
+    />
+  );
+};
+
+export { GlobalStyles, unit, colors };
